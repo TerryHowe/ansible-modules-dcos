e4ae5f210c65c9c3d693d793d3bed8e2e5d542ec
Terry Howe <terrylhowe@gmail.com>
2016-10-18 13:15:05 -0600

    switch to setuptools

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup
 
 files = [
     "ansible/module_utils",
@@ -8,18 +8,19 @@ files = [
 
 long_description = open('README.rst', 'r').read()
 
-setup(name='ansible-modules-dcos',
-      version='1.0.7',
-      description='DCOS Ansible Modules',
-      long_description=long_description,
-      url='https://github.com/TerryHowe/ansible-modules-dcos',
-      author='Kevin Wood,Terry Howe',
-      author_email='kevin.wood@example.com',
-      license='MIT',
-      packages=files,
-      requires = [
-        'ansible (>2.0.0)',
-        'dcoscli (>0.4.5)',
+setup(
+    name='ansible-modules-dcos',
+    version='1.0.8',
+    description='DCOS Ansible Modules',
+    long_description=long_description,
+    url='https://github.com/TerryHowe/ansible-modules-dcos',
+    author='Kevin Wood,Terry Howe',
+    author_email='kevin.wood@example.com',
+    license='MIT',
+    packages=files,
+    install_requires = [
+        'ansible>=2.0.0',
+        'dcoscli>=0.4.5',
         'toml',
-      ],
+    ],
 )
