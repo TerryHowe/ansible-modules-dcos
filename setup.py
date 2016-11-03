60a42811ca10f01ed0bdfae694490fa72468dead
Terry Howe <terrylhowe@gmail.com>
2016-11-03 14:45:45 -0600

    use py modules

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+py_files=[
+    "ansible/module_utils/dcos",
+    "ansible/plugins/lookup/dcos_token",
+    "ansible/plugins/lookup/dcos_token_header",
+]
 files = [
     "ansible/modules/dcos",
 ]
@@ -9,13 +14,14 @@ long_description = open('README.rst', 'r').read()
 
 setup(
     name='ansible-modules-dcos',
-    version='1.2.2',
+    version='1.2.3',
     description='DCOS Ansible Modules',
     long_description=long_description,
     url='https://github.com/TerryHowe/ansible-modules-dcos',
     author='Kevin Wood,Terry Howe',
     author_email='kevin.wood@example.com',
     license='MIT',
+    py_modules=py_files,
     packages=files,
     install_requires = [
         'ansible>=2.0.0',
